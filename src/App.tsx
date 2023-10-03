a147a4d48b72a82ef0bc895e432fddfdd1e37313
Ihtesham Ul Haq <ihteshamulhaq510@gmail.com>
2023-10-03 23:44:44 +0500

    added authentication and ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import Auth from './Main/Login/Auth'
+import Main from './Main/Main'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from './utils/firebase-config'
+import Header from './Main/Header/Header'
 export default function App() {
+  const [user] = useAuthState(auth)
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>hellow world</h1>
+      <Header />
+      {user ? <Main /> : <Auth />}
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: {
+          primary: '#404258',
+          secondary: '#474E68',
+          third: '#50577A',
+          fourth: '#6B728E',
+        },
+        light: {
+          primary: '#EFFFFD',
+          secondary: '#B8FFF9',
+          third: '#85F4FF',
+          fourth: '#42C2FF',
+        },
+      },
+      boxShadow: {
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+      },
+    },
   },
   plugins: [],
 }
